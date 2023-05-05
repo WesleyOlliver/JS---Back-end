399dfb9c37482510f472101bb76d97c55573e520
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-04 23:40:15 -0300

    Create type-boolean.js

---
+++ type-boolean.js
@@ -0,0 +1,13 @@
+const idade = 12;
+const nome = "Lara";
+const sobrenome = "Ayumi";
+const bacharel = false;
+const dezAnosDepois = "12";
+
+const nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);//Lara Ayumi
+
+const resultado = idade + dezAnosDepois;
+console.log(resultado);//1212
+
+console.log(bacharel == 1);//false
