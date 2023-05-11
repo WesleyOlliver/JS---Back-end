05d0108bf012dff76f96c42e6b8f4d36c3311edf
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-11 00:38:52 -0300

    Update type-number.js

--- type-number.js
+++ type-number.js
@@ -7,3 +7,9 @@ const number1 = 0;
 const number2 = 0;
 
 console.log(number1/number2); //NaN
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10;
+
+console.log(operacao);
