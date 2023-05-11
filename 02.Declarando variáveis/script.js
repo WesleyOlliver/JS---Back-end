3c0f5fab7d4b85a82d6370e5f44ddbbe07c531b2
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-11 01:49:16 -0300

    Update script.js

--- 02.Declarando variáveis/script.js
+++ 02.Declarando variáveis/script.js
@@ -1,8 +1,38 @@
-//var
+//var - muito volátil, mas imprópria para o uso nos dias de hoje (sujeito a muitos bugs).
 
-var altura = 5;
-var comprimento = 7;
+//var altura = 5;
+//var comprimento = 7;
 
-area = altura * comprimento;
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+//let - podemos reatribuir valores a variável, aceitando mudanças no mesmo.
+
+//let forma = 'retângulo';
+//let altura = 5;
+//let comprimento = 7;
+//let area
+
+//if (forma === 'retângulo') {
+//    area = altura * comprimento;
+//} else {
+//    area = (altura * comprimento) / 2;
+//}
+
+//console.log(area);
+
+//const - uma vez descriminado um valor a ela, não se é mais possível realizar alterações.
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
 console.log(area);
-var area;
