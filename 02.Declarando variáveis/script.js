aea45d7e6287cb128aee7bfc484621b18bb1f2c4
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-14 21:24:29 -0300

    Update script.js

--- 02.Declarando variáveis/script.js
+++ 02.Declarando variáveis/script.js
@@ -36,3 +36,17 @@ if (forma === 'quadrado') {
 }
 
 console.log(area);
+
+//Variáveis e a resposta de tudo
+
+var respostaDeTudo = 42;
+let idade = 29;
+const pi = 3.14;
+
+{
+    var respostaDeTudo = 3.14;
+    let idade = 42;
+    const pi = 29;
+    console.log(respostaDeTudo, idade, pi); //3.14, 42, 29
+}
+console.log(respostaDeTudo, idade, pi);//3.14, 29, 3.14
