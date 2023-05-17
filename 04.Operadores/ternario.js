a4f66a535ec4700497b4fd0b20327a4503190b00
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-17 00:53:49 -0300

    Create ternario.js

---
+++ 04.Operadores/ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja!");
+} else {
+    console.log("Suco!");
+}
+
+// Operador ternário
+
+console.log(idadeCliente >= idadeMinima ? "Cerveja!" : "Suco!");
+                     //Comparação          //True     //False
