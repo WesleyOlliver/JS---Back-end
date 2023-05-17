ae86a5827048e69055e9afac6d8338e51edd0dd7
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-17 01:14:18 -0300

    Create template-literal.js

---
+++ 04.Operadores/template-literal.js
@@ -0,0 +1,13 @@
+const nome = "Wesley";
+const idade = 2023-1995;
+const cidadeDeNascimento = 'Capão Bonito';
+
+//const apresentacao = "Me chamo " + nome + ", no momento tenho " + idade + " anos e nasci na cidade de " + cidadeDeNascimento + ".";
+
+//console.log(apresentacao);
+
+// Template de String => ${}
+
+const apresentacao = `Me chamo ${nome}, no momento tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}.`;
+
+console.log(apresentacao);
