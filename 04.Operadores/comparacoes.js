559262e5332d4f776c0e4be1d7271c66d338c864
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-17 01:32:48 -0300

    Update comparacoes.js

--- 04.Operadores/comparacoes.js
+++ 04.Operadores/comparacoes.js
@@ -3,18 +3,57 @@
 const numero = 5;
 const palavra = "5";
 
-console.log(numero == palavra);
+//console.log(numero == palavra);
 
 // === compara o valor e o tipo de dado da variável - comparação implícita
 
-console.log(numero === palavra);
+//console.log(numero === palavra);
 
 // typeof nos dara como informação qual é o tipo do valor da variável "chamada".
 
-console.log(typeof numero);
-console.log(typeof palavra);
+//console.log(typeof numero);
+//console.log(typeof palavra);
 
 // comparação explícita
 
 Number()
 String()
+
+// Praticando os operadores
+
+const a = [];
+const b = 20;
+const c = true;
+const d = '';
+
+//if (b === 20 && c != false && d == 0) {
+//    console.log ('Verdadeiro.');
+//} else {
+//    console.log('Falso.');
+//}
+
+console.log(b === 20 && c != false && d == 0 ? 'Verdadeiro.' : 'Falso.'); // Verdadeiro.
+
+//if (a == 0) {
+//    console.log('Verdadeiro.');
+//} else {
+//    console.log('Falso.');
+//}
+
+console.log(a == 0 ? 'Verdadeiro.' : 'Falso.'); // Verdadeiro.
+
+//if (d) {
+//    console.log('Verdadeiro.');
+//} else {
+//    console.log('Falso.');
+//}
+
+console.log(d ? 'Verdadeiro.' : 'Falso.'); // Falso.
+
+//if (b == '20') {
+//    console.log('Verdadeiro.');
+//} else {
+//    console.log('Falso.');
+//}
+
+console.log(b == '20' ? 'Verdadeiro.' : 'Falso.'); // Verdadeiro.
