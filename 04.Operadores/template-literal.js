eb2e146c9067454f819ebf83de80310478ec92e4
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-18 23:34:07 -0300

    Update template-literal.js

--- 04.Operadores/template-literal.js
+++ 04.Operadores/template-literal.js
@@ -11,3 +11,11 @@ const cidadeDeNascimento = 'Capão Bonito';
 const apresentacao = `Me chamo ${nome}, no momento tenho ${idade} anos e nasci na cidade de ${cidadeDeNascimento}.`;
 
 console.log(apresentacao);
+
+const cliente = "Leo";
+const anos = 2023-2000;
+const bebidaMaior = "cerveja!";
+const bebidaMenor = "suco!";
+const pedido = `${cliente} diz: 'por favor, quero beber ${anos >= 18 ? bebidaMaior : bebidaMenor}'`;
+
+console.log(pedido);
