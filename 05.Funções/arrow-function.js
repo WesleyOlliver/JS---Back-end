ee050acb829616db72ff8a369994ae7c8dde0211
Wesley Ribeiro de Oliveira <85084673+WesleyOlliver@users.noreply.github.com>
2023-05-23 00:45:07 -0300

    Update arrow-function.js

--- 05.Funções/arrow-function.js
+++ 05.Funções/arrow-function.js
@@ -1,14 +1,17 @@
-console.log(apresentar("Wesley"));
+console.log(apresentar("Wesley"));                // Meu nome é Wesley.
 
 function apresentar(nome) {
-    return `Meu nome é ${nome}`;
+    return `Meu nome é ${nome}.`;
 }
 
-const apresentarArrow = nome => `Meu nome é ${nome}`;
+// Arrow function: este é o terceiro método para criar uma função no JavaScript (muito usado na geração de "objetos"), tendo os mesmo principios basicos da "expressão de função", onde ela fica compacta no código (usando menos linhas), não possui uma nome para a declaração e hoistingh (por se tratar de uma variável).
+
+// Para criarmos esse novo método devemos atribuir a flecha (=>) após o identificador da constante, em seguidas os parâmetros da função.
+const apresentarArrow = nome => `Meu nome é ${nome}.`;
 const soma = (num1, num2) => num1 + num2;
 
-console.log(apresentarArrow("Wesley"));
-console.log(soma(5, 9));
+console.log(apresentarArrow("Wesley"));          // => Meu nome é Wesley.
+console.log(soma(5, 9));                         // => 5 + 9 = 14
 
 const somaNumerosPequenos = (num1, num2) => {
     if (num1 > 10 || num2 > 10) {
@@ -18,5 +21,5 @@ const somaNumerosPequenos = (num1, num2) => {
     }
 }
 
-console.log(somaNumerosPequenos(15, 8));
-console.log(somaNumerosPequenos(6, 3));
+console.log(somaNumerosPequenos(15, 8));         // => Somente números de 0 a 9.
+console.log(somaNumerosPequenos(6, 3));          // => 6 + 3 = 9
